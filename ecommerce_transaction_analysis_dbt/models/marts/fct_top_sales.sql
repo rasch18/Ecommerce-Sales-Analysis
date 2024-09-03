1f6abd8a82152cb9b931f1f99f5a2757938ce3d0
Raschelle <raschellecossid00@gmail.com>
2024-09-04 00:30:25 +0800

    Update query targeting top items regardless of unit prices

--- ecommerce_transaction_analysis_dbt/models/marts/fct_top_sales.sql
+++ ecommerce_transaction_analysis_dbt/models/marts/fct_top_sales.sql
@@ -10,26 +10,33 @@ with transactions_table as (
     From {{ ref('int_transactions_invoice') }} a
     Left join {{ ref('int_transactions_invoice_items') }} b on a.invoice_no = b.invoice_no
     Where a.invoice_no not in (Select invoice_no from {{ ref('int_transactions_cancelled') }}) -- make sure to not include cancelled items
-    
     -- Get total sum of quantities per item
-    ), quantities as (
+    ), quantities as ( 
         Select 
             stock_code,
             description, 
             unit_price,
-            sum(quantity) as total_quantity
+            sum(quantity) as per_item_quantity
         From transactions_table
         Where description is not null
         Group by 1,2,3
-        Order by total_quantity desc
 
-    )
+    ), item_sales_amount as ( -- same stock codes with different unit prices
       Select 
         stock_code,
         description,
         unit_price,
-        total_quantity,
-        (total_quantity * unit_price) as total_sales_amount
+        per_item_quantity,
+        (per_item_quantity * unit_price) as per_item_sales_amount
       From quantities
       Group by 1,2,3,4
+
+    ) 
+      Select 
+        stock_code,
+        description,
+        sum(per_item_quantity) as total_quantity,
+        sum(per_item_sales_amount) as total_sales_amount
+      From item_sales_amount
+      Group by 1,2
       Order by total_sales_amount desc
