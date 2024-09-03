8e658b16f7265912b4f8c085cce95348bff4a0c6
Raschelle <raschellecossid00@gmail.com>
2024-09-04 02:33:41 +0800

    Update extracted year from invoice to date data type

--- ecommerce_transaction_analysis_dbt/models/marts/fct_annual_total_sales.sql
+++ ecommerce_transaction_analysis_dbt/models/marts/fct_annual_total_sales.sql
@@ -1,5 +1,5 @@
 Select 
-  year(invoice_date) as invoice_year, -- get year data
+  MAKE_DATE(YEAR(invoice_date), 1, 1) as invoice_year, -- get year data
   sum(total_quantity) as annual_total_quantity, -- aggregate all quantities
   sum(total_sales_amount) as annual_total_amount -- aggregate sales amount
 From {{ ref('fct_daily_total_sales') }} -- reference to daily sales
