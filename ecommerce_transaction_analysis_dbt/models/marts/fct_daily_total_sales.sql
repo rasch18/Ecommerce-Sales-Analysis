608e97423802d8480dbde8e6d51794f32eccbd58
Raschelle <raschellecossid00@gmail.com>
2024-09-04 02:03:13 +0800

    Update query with aggregating sales amount per product regardless of price amount

--- ecommerce_transaction_analysis_dbt/models/marts/fct_daily_total_sales.sql
+++ ecommerce_transaction_analysis_dbt/models/marts/fct_daily_total_sales.sql
@@ -4,19 +4,27 @@ with total_quantity_no as (
     a.stock_code,
     a.description,
     a.unit_price, -- though some items are under the same stock code, they have each price amount
-    sum(a.quantity) as total_quantity -- sum number of quantities
+    sum(a.quantity) as total_product_quantity -- sum number of quantities
   From {{ ref('int_transactions_invoice_items') }} a
   Left join {{ ref('int_transactions_invoice') }} b on a.invoice_no = b.invoice_no -- incoporate date field
   Where a.invoice_no not in (Select invoice_no from {{ ref('int_transactions_cancelled') }})
   Group by 1,2,3,4
 
-)
+), sales_per_item_per_day as (
   Select 
     invoice_date,
     stock_code,
     description,
-    total_quantity,
+    total_product_quantity,
     unit_price,
-    SUM(total_quantity * unit_price) AS total_sales_amount  -- calculate the total sales per item
+    SUM(total_product_quantity * unit_price) AS total_product_sales_amount  -- calculate the total sales per item
   From total_quantity_no
   Group by 1,2,3,4,5
+
+)
+  Select 
+    invoice_date,
+    sum(total_product_quantity) as total_quantity, -- get overall quantity of items per day
+    sum(total_product_sales_amount) as total_sales_amount -- get overall sales of items per day
+  From sales_per_item_per_day
+  Group by 1
