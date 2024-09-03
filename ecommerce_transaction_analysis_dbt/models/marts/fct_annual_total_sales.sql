e7894e19cc6b640504ac0d2a07f9f3afaddcc601
Raschelle <raschellecossid00@gmail.com>
2024-09-04 02:03:55 +0800

    Add annual total sales table

---
+++ ecommerce_transaction_analysis_dbt/models/marts/fct_annual_total_sales.sql
@@ -0,0 +1,6 @@
+Select 
+  year(invoice_date) as invoice_year, -- get year data
+  sum(total_quantity) as annual_total_quantity, -- aggregate all quantities
+  sum(total_sales_amount) as annual_total_amount -- aggregate sales amount
+From {{ ref('fct_daily_total_sales') }} -- reference to daily sales
+Group by 1
