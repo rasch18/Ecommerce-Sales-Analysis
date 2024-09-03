624ff0d07ba8297abaeb89d68455f39864a9fd2b
Raschelle <raschellecossid00@gmail.com>
2024-09-04 02:05:05 +0800

    Update query by aggregating sales per item, price amount, and country

--- ecommerce_transaction_analysis_dbt/models/marts/fct_highest_sales_per_country.sql
+++ ecommerce_transaction_analysis_dbt/models/marts/fct_highest_sales_per_country.sql
@@ -1,36 +1,50 @@
 with transaction_table as ( -- get country details
   Select 
-    a.invoice_no,
     a.stock_code,
     a.description,
     a.unit_price,
     a.quantity,
-    c.country
-  From int_transactions_invoice_items a
-  Left join int_transactions_invoice b on a.invoice_no = b.invoice_no
-  Left join int_transactions_customer c on b.customer_id = c.customer_id
-  Where a.invoice_no not in (Select invoice_no from int_transactions_cancelled)
+    b.country
+  From {{ ref('int_transactions_invoice_items') }} a
+  Left join {{ ref('int_transactions_invoice') }} b on a.invoice_no = b.invoice_no
+  Where a.invoice_no not in (Select invoice_no from {{ ref('int_transactions_cancelled') }}) --, exclude canceled transactions
   Order by 1 desc
 
--- Aggregates total sales for each item in each country, filtering out canceled transactions
-), cte_sales as ( -- calculate sum per itemn stock code, and country
+-- Aggregate total quantity in each unit price of an item
+), cte_aggregate_quantity_per_country as ( -- calculate sum per itemn stock code, and country
   Select 
     country,
     stock_code,
     description,
-    sum(quantity * unit_price) as total_sales 
+    unit_price,
+    sum(quantity) as total_product_quantity
   From transaction_table
-  Where country is not null
-  Group by 1,2,3
+  Group by 1,2,3,4
 
--- Ranks items within each country based on their total sales
-)
+-- Aggregate sales amount for each unit price per item
+), cte_aggregate_total_sales_amount_per_unit_price as (
   Select 
     country,
     stock_code,
     description,
-    total_sales,
-    row_number() over(partition by country order by total_sales desc) as item_ranking
-  From cte_sales
+    unit_price,
+    sum(total_product_quantity * unit_price) as total_sales_amount
+  From cte_aggregate_quantity_per_country
+  Group by 1,2,3,4
+
+-- Aggregate sales amount for each item in each country
+), cte_top_item_per_country as ( 
+  Select
+    country,
+    description,
+    sum(total_sales_amount) as total_country_sales_amount,
+    -- Ranks items within each country based on their total sales
+    row_number() over(partition by country order by total_country_sales_amount desc) as item_ranking
+  From cte_aggregate_total_sales_amount_per_unit_price
+  Group by 1,2
   Qualify item_ranking = 1 -- get only the highest selling item based on sales
   Order by 1 asc
+
+)
+  Select * exclude(item_ranking)
+  From cte_top_item_per_country
